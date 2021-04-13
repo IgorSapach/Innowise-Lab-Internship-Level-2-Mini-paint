6d60dad2ee036851696a80e1a94764c72df4fdbc
Igor <49233393+IgorSapach@users.noreply.github.com>
2021-04-13 17:47:34 +0300

    added draw area with color picker and size picker

--- src/App.vue
+++ src/App.vue
@@ -23,3 +23,18 @@ export default defineComponent({
   },
 });
 </script>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  font-size: 16px;
+  color: #bebebe;
+  font-family: Helvetica, Arial, Lucida Grande, sans-serif;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
