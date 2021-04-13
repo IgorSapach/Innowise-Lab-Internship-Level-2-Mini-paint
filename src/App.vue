a7763f109c38ab9e2d6cbdfcc3bfe722ed771d76
Igor <49233393+IgorSapach@users.noreply.github.com>
2021-04-13 14:58:45 +0300

    added redirection if have key in local storage

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,23 @@
 </template>
 
 <script>
-export default {
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import router from "@/router";
+
+export default defineComponent({
   name: "App",
-};
+  setup() {
+    const store = useStore();
+    if (localStorage.getItem("userId")) {
+      let userId = JSON.parse(localStorage.getItem("userId"));
+      store.commit("setUserId", userId.userId);
+      store.commit("setUserIsAuth", true);
+      router.push({
+        name: "paint",
+        params: { uid: store.getters.userId },
+      });
+    }
+  },
+});
 </script>
