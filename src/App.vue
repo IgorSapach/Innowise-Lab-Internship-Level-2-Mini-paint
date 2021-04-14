83fa8c6896abb3c08d7a3a9b118012eca08f9063
Igor <49233393+IgorSapach@users.noreply.github.com>
2021-04-14 10:04:44 +0300

    fixed SignUp call, moved env config, moved init call

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,11 @@
 
 <script>
 import { defineComponent } from "vue";
-import { useStore } from "vuex";
-import router from "@/router";
 
 export default defineComponent({
   name: "App",
-  setup() {
-    const store = useStore();
-    if (localStorage.getItem("userId")) {
-      let userId = JSON.parse(localStorage.getItem("userId"));
-      store.commit("setUserId", userId.userId);
-      store.commit("setUserIsAuth", true);
-      router.push({
-        name: "paint",
-        params: { uid: store.getters.userId },
-      });
-    }
+  mounted() {
+    this.$store.dispatch("init");
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import firebase from "firebase";
 import dotenv from "dotenv";
 
 createApp(App).use(store).use(router).mount("#app");
 dotenv.config();
-
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATA_BASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID,
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
-};
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-export default firebaseApp;
